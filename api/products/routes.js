fcac5eb0327a5e8ce97c0d708f8dd92797000b67
Henrique Silverio <henrique-silverio@hotmail.com>
2019-06-16 18:21:32 -0300

    Refactor to async/await

--- api/products/routes.js
+++ api/products/routes.js
@@ -1,58 +1,52 @@
+import to from '../../lib/await-to'
+
 import Product from './model'
 
 export default (api) => {
   api.route('/api/products')
-    .get((req, res) => {
-      Product.find((error, products) => {
-        if(error) {
-          res.json({ 'error': 'Cannot GET products.' })
-        }
-        res.json(products)
-      })
+    .get(async (req, res) => {
+      const [ error, products ] = await to(Product.find())
+      if(error) {
+        res.json({ 'error': 'Cannot GET products.' })
+      }
+      res.json(products)
     })
-    .post((req, res) => {
+    .post(async (req, res) => {
       const Product = new Product({
         'name': req.body.name,
         'code': req.body.code
       })
-
-      Product.save((error, product) => {
-        if(error) {
-          res.json({ 'error': 'Cannot CREATE product.' })
-        }
-        res.json(product)
-      })
+      const [ error, product ] = await Product.save()
+      if(error) {
+        res.json({ 'error': 'Cannot CREATE product.' })
+      }
+      res.json(product)
     })
 
   api.route('/api/products/:id')
-    .get((req, res) => {
-      Product.findById(req.params.id, (error, product) => {
-        if(error) {
-          res.json({ 'error': 'Product not found.' })
-        }
-        res.json(product)
-      })
+    .get(async (req, res) => {
+      const [ error, product] = await to(Product.findById(req.params.id))
+      if(error) {
+        res.json({ 'error': 'Product not found.' })
+      }
+      res.json(product)
     })
-    .put((req, res) => {
+    .put(async (req, res) => {
       const id = req.params.id
       const data = { 'name': req.body.name, 'code': req.body.code }
       const options = { 'new': true }
-
-      Product.findByIdAndUpdate(id, data, options, (error, product) => {
-        if(error) {
-          res.json('Cannot UPDATE product.')
-        }
-        res.json(product)
-      })
+      const [ error, product ] = await to(Product.findByIdAndUpdate(id, data, options))
+      if(error) {
+        res.json('Cannot UPDATE product.')
+      }
+      res.json(product)
     })
-    .delete((req, res) => {
+    .delete(async (req, res) => {
       const id = req.params.id
-
-      Product.findByIdAndRemove(id, (error, product) => {
-        if(error) {
-          res.json({ 'error': 'Cannot DELETE product.' })
-        }
-        res.json(product)
-      })
+      const [ error, product ] = await to(Product.findByIdAndRemove(id))
+      if(error) {
+        res.json({ 'error': 'Cannot DELETE product.' })
+      }
+      res.json(product)
     })
 }
