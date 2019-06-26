bec13f3cd197850431a563e569b7ec505b44c572
Henrique Silverio <henrique-silverio@hotmail.com>
2019-06-26 20:43:33 -0300

    Solve 'ensureIndex' warning

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export default () => {
   const db = mongoose.connection
-  const options = { useNewUrlParser: true, useFindAndModify: false }
+  const options = { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true }
   db.on('error', console.error.bind(console, 'Connection error:'))
   db.once('open', () => console.log('Connected to database!'))
   return mongoose.connect('mongodb://localhost/api', options)
