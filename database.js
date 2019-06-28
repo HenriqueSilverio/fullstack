e5ec3123173cf76935d7759986e6dd56b3313f35
Henrique Silverio <henrique-silverio@hotmail.com>
2019-06-28 12:59:44 -0300

    Dynamic connection URI

--- database.js
+++ database.js
@@ -1,9 +1,17 @@
 import mongoose from 'mongoose'
 
+const DB_USER = process.env.DB_USER || ''
+const DB_PASS = process.env.DB_PASS || ''
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_PORT = process.env.DB_PORT || '27017'
+const DB_NAME = process.env.DB_NAME || 'api'
+
 export default () => {
   const db = mongoose.connection
+  const credentials = DB_USER && DB_PASS ? `${DB_USER}:${DB_PASS}@` : ''
+  const uri = `mongodb://${credentials}${DB_HOST}:${DB_PORT}/${DB_NAME}`
   const options = { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true }
   db.on('error', console.error.bind(console, 'Connection error:'))
   db.once('open', () => console.log('Connected to database!'))
-  return mongoose.connect('mongodb://localhost/api', options)
+  return mongoose.connect(uri, options)
 }
